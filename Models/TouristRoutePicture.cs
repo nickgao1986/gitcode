fe3ed65a2ee37f011fba1875c7ba80423c60f46e
nick <youjiancau@163.com>
2020-09-19 09:45:01 +0800

    fix1

--- Models/TouristRoutePicture.cs
+++ Models/TouristRoutePicture.cs
@@ -10,7 +10,7 @@ namespace XieChengAPI.Models
         public Guid Id { get; set; }
         public string Url { get; set; }
         public Guid TouristRouteId { get; set; }
-        public TouristRoute tourist1;
+        public TouristRoute tourist;
         
     }
 }
